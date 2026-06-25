d253b686b10279f77b4fee35524390a384c46ea9
Aku sunsin <juarajp43@gmail.com>
2026-06-25 12:42:14 +0700

    Remove
    Removed the 'Download Manual Book' section from the dashboard.

--- resources/views/intern/dashboard.blade.php
+++ resources/views/intern/dashboard.blade.php
@@ -206,18 +206,6 @@
                         </div>
                         <i class="fas fa-chevron-right text-gray-400 group-hover:text-orange-500"></i>
                     </a>
-
-                    <!-- Download Manual Book -->
-                    <a href="{{ asset('manuals/Manual Book - ARSIPIN.pdf') }}" target="_blank"
-                        class="flex items-center justify-between p-4 bg-orange-50 hover:bg-orange-100 rounded-lg transition-colors group">
-                        <div class="flex items-center">
-                            <div class="w-10 h-10 bg-orange-500 rounded-lg flex items-center justify-center mr-3">
-                                <i class="fas fa-book text-white"></i>
-                            </div>
-                            <span class="font-medium text-gray-900">Download Manual Book</span>
-                        </div>
-                        <i class="fas fa-download text-gray-400 group-hover:text-orange-500"></i>
-                    </a>
                 </div>
             </div>
 
